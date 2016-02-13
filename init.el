78552908f36bc2fd69e0fae2cacacc66be014942
George Thomas <iamgeorgethomas@gmail.com>
2016-02-13 13:12:43 +0530

    More packages and definitions

--- init.el
+++ init.el
@@ -15,6 +15,23 @@
 (blink-cursor-mode -1)
 (scroll-bar-mode -1)
 
+;; Newline at end of file
+(setq require-final-newline t)
+
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; enable y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq select-enable-clipboard t) ;; Enable copy/past-ing from clipboard
+(setq system-uses-terminfo nil) ;; Fix weird color escape sequences
+
+;; Ask for confirmation before closing emacs
+(setq confirm-kill-emacs 'yes-or-no-p)
+;; Always reload the file if it changed on disk
+(global-auto-revert-mode 1)
 ;; nice scrolling
 (setq scroll-margin 0
       scroll-conservatively 100000
@@ -24,11 +41,24 @@
 (setq inhibit-startup-message t)
 
 ;; enable melpa packages
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
 
 ;; start emacs in fullscreen
 (toggle-frame-fullscreen)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-company-mode t)
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (ruby-block scss-mode ac-haskell-process diminish spaceline smartparens yari robe xclip yaml-mode fill-column-indicator crux anzu emmet-mode helm-package helm-projectile haml-mode org markdown-mode helm-ag helm-flycheck helm-rails magit json-mode flycheck multiple-cursors zenburn-theme spacemacs-theme projectile js2-mode helm guru-mode company coffee-mode aggressive-indent ace-window))))
 
 ;; switch mac control and meta buttons
 (setq mac-command-modifier 'control)
@@ -47,7 +77,16 @@
 
 ;; load spacemacs theme
 (load-theme 'spacemacs-dark t)
+(require 'spaceline-config)
+(spaceline-emacs-theme)
 
+;; whitespace-mode config
+(require 'whitespace)
+(setq whitespace-line-column 80) ;; limit line length
+(setq whitespace-style '(face tabs empty trailing lines-tail))
+(global-whitespace-mode t)
+
+(require 'ruby-end)
 ;; enable crux
 (require 'crux)
 (global-set-key (kbd "C-c I") #'crux-find-user-init-file)
@@ -58,6 +97,13 @@
 (global-set-key (kbd "C-c r") #'crux-rename-file-and-buffer)
 (global-set-key (kbd "C-c S") #'crux-find-shell-init-file)
 (global-set-key (kbd "s-k") #'crux-kill-whole-line)
+(global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
+
+;; helm-mode by default
+(require 'helm)
+(require 'helm-config)
+(helm-mode 1)
+(global-company-mode)
 
 ;; Helm-bindings for common actions
 (global-set-key (kbd "M-x") 'helm-M-x)
@@ -82,9 +128,31 @@
 ;; enable projectile-globally
 (projectile-global-mode)
 ;; enable flycheck-globally
-(global-flycheck-mode)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; anzu mode globally
+(require 'anzu)
+(global-anzu-mode)
+(diminish 'anzu-mode)
+(global-set-key (kbd "M-%") 'anzu-query-replace)
+(global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
+
+
+;; Start eshell or switch to it if it's active.
+(global-set-key (kbd "C-x m") 'eshell)
+
+;; Start a regular shell
+(global-set-key (kbd "C-x M-m") 'shell)
+
+;; set rubocop executable
+(setq flycheck-ruby-rubocop-executable "~/.rbenv/shims/rubocop")
+
 ;; enable helm bindings for projectile
 (helm-projectile-on)
+(spaceline-helm-mode)
+
+;; global-smartparens
+(smartparens-global-mode)
 
 ;; Enable multiple-cursors-mode
 (require 'multiple-cursors)
@@ -92,15 +160,11 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+;; Highlight the current line
+(global-hl-line-mode +1)
+
 ;; (setq flycheck-eslintrc "~/.eslintrc")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (emmet-mode helm-package helm-projectile haml-mode org markdown-mode helm-ag helm-flycheck helm-rails magit json-mode flycheck robe multiple-cursors zenburn-theme spacemacs-theme rubocop projectile js2-mode helm guru-mode company coffee-mode aggressive-indent ace-window))))
+
 
 (provide 'init)
 ;;; init.el ends here
