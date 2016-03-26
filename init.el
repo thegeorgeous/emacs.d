4a9d73541fdd9fc29ef59520392041ccd632764c
George Thomas <iamgeorgethomas@gmail.com>
2016-03-26 14:29:42 +0530

    Add org-mode elpa packages

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 ;; fix the emacs ui
