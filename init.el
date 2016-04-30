be6580278bc9de82776b348685c5f4f5d89f8d91
George Thomas <iamgeorgethomas@gmail.com>
2016-04-30 22:54:24 +0530

    Use use-package for a cleaner config file

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/") t)
+             '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 ;; fix the emacs ui
@@ -39,14 +39,16 @@
 ;; Ask for confirmation before closing emacs
 (setq confirm-kill-emacs 'yes-or-no-p)
 
-(require 'exec-path-from-shell)
-(setq exec-path-from-shell-check-startup-files nil)
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+(use-package exec-path-from-shell
+  :config
+  (setq exec-path-from-shell-check-startup-files nil)
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize)))
 
 ;; Always reload the file if it changed on disk
 (global-auto-revert-mode 1)
 (diminish 'auto-revert-mode)
+
 ;; nice scrolling
 (setq scroll-margin 0
       scroll-conservatively 100000
@@ -63,11 +65,14 @@
 
 ;; start emacs in fullscreen
 (toggle-frame-fullscreen)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
  '(custom-safe-themes
    (quote
     ("8aa7eb0cc23931423f719e8b03eb14c4f61aa491e5377073d6a55cba6a7bc125" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "824c02fec7dfb3affca5f5a3b8dddfb8ded6e23ba82b8a1c72191d66f2418b8c" "20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" default)))
@@ -77,14 +82,15 @@
  '(org-agenda-files (quote ("~/src/btt/ehq/participation/tasks.org")))
  '(package-selected-packages
    (quote
-    (wc-goal-mode wc-mode fill-column-indicator exec-path-from-shell ack erc-colorize projectile-rails monokai-theme powerline auto-compile spaceline rspec-mode web-mode gotham-theme rainbow-mode company-inf-ruby beacon zop-to-char scss-mode diminish smartparens yari robe xclip yaml-mode crux anzu emmet-mode helm-package helm-projectile haml-mode org markdown-mode helm-ag helm-flycheck helm-rails magit json-mode flycheck multiple-cursors zenburn-theme spacemacs-theme projectile js2-mode helm guru-mode company coffee-mode aggressive-indent ace-window)))
+    (use-package paradox wc-goal-mode wc-mode fill-column-indicator exec-path-from-shell ack erc-colorize projectile-rails monokai-theme powerline auto-compile spaceline rspec-mode web-mode rainbow-mode company-inf-ruby beacon zop-to-char scss-mode diminish smartparens yari robe xclip yaml-mode crux anzu emmet-mode helm-package helm-projectile haml-mode org markdown-mode helm-ag helm-flycheck helm-rails magit json-mode flycheck multiple-cursors zenburn-theme spacemacs-theme projectile js2-mode helm guru-mode company coffee-mode aggressive-indent ace-window)))
+ '(paradox-github-token t)
  '(send-mail-function (quote mailclient-send-it))
  '(show-smartparens-global-mode t))
-(with-eval-after-load 'helm
-  (diminish 'helm-mode))
+
 ;; switch mac control and meta buttons
-(setq mac-command-modifier 'control)
-(setq mac-control-modifier 'super)
+(when (eq system-type 'darwin)
+  (setq mac-command-modifier 'control)
+  (setq mac-control-modifier 'super))
 
 ;; make cursor a line
 (setq-default cursor-type 'bar)
@@ -101,9 +107,11 @@
 (load-theme 'monokai t)
 
 ;; Use the default powerline
-(require 'powerline)
-(powerline-default-theme)
-(setq powerline-default-separator 'wave)
+(use-package powerline
+  :config
+  (powerline-default-theme)
+  (setq powerline-default-separator 'wave))
+
 ;; better colours for colorline separator
 (setq ns-use-srgb-colorspace nil)
 
@@ -127,16 +135,13 @@
 (global-set-key [(control shift down)]  'move-line-down)
 
 ;; whitespace-mode config
-(require 'whitespace)
-(setq whitespace-line-column 80) ;; limit line length
-;; Whitespace mode styles to be used
-(setq whitespace-style '(face tabs empty trailing lines-tail))
-;; color to be shown if line crosses the line length limit
-;;(set-face-attribute 'whitespace-line nil :background nil :foreground "#e4d836")
-(global-whitespace-mode t)
-;; Remove trailing whitespace on save
-(add-hook 'before-save-hook 'whitespace-cleanup t)
-(diminish 'global-whitespace-mode)
+(use-package whitespace
+  :diminish global-whitespace-mode
+  :config
+  (setq whitespace-line-column 80)
+  (setq whitespace-style '(face tabs empty trailing lines-tail))
+  (global-whitespace-mode t)
+  (add-hook 'before-save-hook 'whitespace-cleanup t))
 
 ;; CamelCase aware editing
 (add-hook 'prog-mode-hook 'subword-mode)
@@ -145,26 +150,28 @@
 (global-subword-mode +1)
 
 ;; enable crux
-(require 'crux)
-(global-set-key (kbd "C-c I") #'crux-find-user-init-file)
-(global-set-key (kbd "C-c D") #'crux-delete-file-and-buffer)
-(global-set-key (kbd "C-c s") #'crux-transpose-windows)
-(global-set-key (kbd "C-c k") #'crux-kill-other-buffers)
-(global-set-key (kbd "s-j") #'crux-top-join-line)
-(global-set-key (kbd "C-c R") #'crux-rename-file-and-buffer)
-(global-set-key (kbd "C-c S") #'crux-find-shell-init-file)
-(global-set-key (kbd "s-k") #'crux-kill-whole-line)
-(global-set-key (kbd "C-c n") #'crux-cleanup-buffer-or-region)
-(global-set-key (kbd "C-c TAB") #'crux-indent-rigidly-and-copy-to-clipboard)
-(global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
-
+(use-package crux
+  :bind (("C-c I" . crux-find-user-init-file)
+	 ("C-c D" . crux-delete-file-and-buffer)
+	 ("C-c s" . crux-transpose-windows)
+	 ("C-c k" . crux-kill-other-buffers)
+	 ("s-j" . crux-top-join-line)
+	 ("C-c R" . crux-rename-file-and-buffer)
+	 ("C-c S" . crux-find-shell-init-file)
+	 ("s-k" . crux-kill-whole-line)
+	 ("C-c n" . crux-cleanup-buffer-or-region)
+	 ("C-c TAB" . crux-indent-rigidly-and-copy-to-clipboard)
+	 ("C-a" . crux-move-beginning-of-line)))
 
 ;; Package List shortcut
-(global-set-key (kbd "C-x p") 'package-list-packages)
+(bind-keys :prefix-map package-map
+	  :prefix "C-x p"
+	  ("p" . package-list-packages)
+	  ("P" . paradox-list-packages))
 
 ;; Emacs-Lisp mode keys
-(eval-after-load "Emacs-Lisp"
-  (define-key emacs-lisp-mode-map (kbd "C-c C-b") 'eval-buffer))
+(bind-keys :map emacs-lisp-mode-map
+	   ("C-c C-b" . eval-buffer))
 
 ;; Org-mode
 (with-eval-after-load 'org
@@ -183,60 +190,71 @@
 
 
 ;; helm-mode by default
-(require 'helm)
-(require 'helm-config)
-(helm-mode 1)
-(global-company-mode)
-(diminish 'company-mode)
+;; (require 'helm)
+;; (require 'helm-config)
+(use-package helm
+  :diminish helm-mode
+  :config
+  (helm-mode 1)
+)
+
+;; Use company
+(use-package company
+  :diminish company-mode
+  :config
+  (global-company-mode))
 
 ;; Helm-bindings for common actions
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-c f") 'helm-recentf)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(bind-key "M-x" 'helm-M-x)
+(bind-key "C-x C-f" 'helm-find-files)
+(bind-key "C-c f" 'helm-recentf)
+(bind-key "C-x b" 'helm-mini)
+(bind-key "C-x C-b" 'helm-buffers-list)
 
 ;; magit keys
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-c b") 'magit-blame)
+(bind-key "C-x g" 'magit-status)
+(bind-key "C-c b" 'magit-blame)
 
 ;; avy and ace-window
-(global-set-key (kbd "C-c j") 'avy-goto-word-or-subword-1)
-(global-set-key (kbd "s-.") 'avy-goto-word-or-subword-1)
-(global-set-key (kbd "s-w") 'ace-window)
+(bind-key "C-c j" 'avy-goto-word-or-subword-1)
+(bind-key "s-." 'avy-goto-word-or-subword-1)
+(bind-key "s-w" 'ace-window)
 
 ;; enable guru-global-mode
-(require 'guru-mode)
-(guru-global-mode +1)
-(setq guru-warn-only t)
-(diminish 'guru-mode)
+(use-package guru-mode
+  :diminish guru-mode
+  :config
+  (guru-global-mode +1)
+  (setq guru-warn-only t))
 
 ;; enable projectile-globally
 (projectile-global-mode)
 ;; enable projetile-rails-mode
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
-(diminish 'projectile-rails-mode)
-;; enable flycheck-globally
-(add-hook 'after-init-hook #'global-flycheck-mode)
+(with-eval-after-load 'projectile-rails
+  (diminish 'projectile-rails-mode))
 
 ;; anzu mode globally
-(require 'anzu)
-(global-anzu-mode)
-(diminish 'anzu-mode)
-(global-set-key (kbd "M-%") 'anzu-query-replace)
-(global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
-
+(use-package anzu
+  :diminish anzu-mode
+  :config
+  (global-anzu-mode)
+  :bind (("M-%" . anzu-query-replace)
+	 ("C-M-%" . anzu-query-replace-regexp)))
 
 ;; Start eshell or switch to it if it's active.
-(global-set-key (kbd "C-x m") 'eshell)
+(bind-key "C-x m" 'eshell)
 
 ;; Start a regular shell
-(global-set-key (kbd "C-x M-m") 'shell)
+(bind-key "C-x M-m" 'shell)
 
 ;; set flycheck configurations
-(require 'flycheck)
-(setq flycheck-ruby-rubocop-executable "~/.rbenv/shims/rubocop") ;; set rubocop executable
-(setq flycheck-eslintrc "~/.eslintrc") ;; set config file for eslint
+(use-package flycheck
+  :init
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  :config
+  (setq flycheck-ruby-rubocop-executable "~/.rbenv/shims/rubocop")
+  (setq flycheck-eslintrc "~/.eslintrc"))
 
 ;; enable helm bindings for projectile
 (helm-projectile-on)
@@ -249,27 +267,33 @@
 (diminish 'smartparens-mode)
 
 ;; Enable multiple-cursors-mode
-(require 'multiple-cursors)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(use-package multiple-cursors
+  :bind (("C-<" . mc/mark-previous-like-this)
+	 ("C->" . mc/mark-next-like-this)
+	 ("C-c C-<" . mc/mark-all-like-this)))
 
 ;; Highlight the current line
 (global-hl-line-mode +1)
 
 ;; Enable robe-mode
-(add-hook 'ruby-mode-hook 'robe-mode)
-(diminish 'robe-mode)
+(use-package ruby-mode
+  :config
+  (add-hook 'ruby-mode-hook 'robe-mode))
 
-(diminish 'rspec-mode)
+(use-package robe
+  :diminish robe-mode)
+
+(use-package rspec-mode
+  :diminish rspec-mode)
 
 ;; Use web mode for erb templates
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(use-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)))
 
 ;; For when current line and column display
 ;; is out of the screen
-(global-set-key (kbd "C-c l") 'what-line)
+(bind-key "C-c l" 'what-line)
 
 (provide 'init)
 ;;; init.el ends here
