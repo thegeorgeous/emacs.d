67187e04a2e35a0c9f5eb6a6eb5c3cbca7a96d5e
George Thomas <iamgeorgethomas@gmail.com>
2016-05-19 09:34:10 +0530

    Better use of markdown mode

--- init.el
+++ init.el
@@ -326,7 +326,12 @@
 
 ;; Add markdown-mode
 (use-package markdown-mode
-  :ensure t)
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 (use-package yaml-mode
   :ensure t)
