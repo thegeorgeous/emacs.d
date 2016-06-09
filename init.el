e60d230acda4ac6af25b82b742583d6a1d50b7b5
George Thomas <iamgeorgethomas@gmail.com>
2016-06-10 03:24:51 +0530

    Larger font size, less strain for the eyes

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 160 :family "Inconsolata")))))
+ '(default ((t (:height 180 :family "Inconsolata")))))
 
 ;; load zenburn theme
 (use-package zenburn-theme
