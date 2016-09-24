c5099b6da96040df1997ba36452beec0836faa2f
George Thomas <iamgeorgethomas@gmail.com>
2016-09-24 21:12:31 +0530

    Set font-size to 16 points

--- init.el
+++ init.el
@@ -102,7 +102,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 180 :family "Inconsolata")))))
+ '(default ((t (:height 160 :family "Inconsolata")))))
 
 
 ;; load zenburn theme
