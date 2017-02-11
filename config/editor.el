74a52f432de6a2d45cdc8a62a2684dcd86863894
George Thomas <iamgeorgethomas@gmail.com>
2017-02-11 11:38:08 +0530

    Require 'auto-complete-config' before enabling it

--- config/editor.el
+++ config/editor.el
@@ -57,6 +57,7 @@
   :ensure t
   :diminish auto-complete-mode
   :config
+  (require 'auto-complete-config)
   (ac-config-default)
   (setq ac-ignore-case nil)
   (add-to-list 'ac-modes 'enh-ruby-mode))
