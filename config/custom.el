b91de8755de76caa4ad072055339a657752f5ee1
George Thomas <iamgeorgethomas@gmail.com>
2017-02-16 19:52:21 +0530

    Let Custom add package-selected-packages variable

--- config/custom.el
+++ config/custom.el
@@ -7,11 +7,21 @@
 ;; set font family and size
 (set-face-attribute 'default nil :height 140 :family "Fira Code")
 
-(setq package-selected-packages
-   (quote
-    (zenburn-theme yasnippet yaml-mode web-mode use-package smartparens rspec-mode rainbow-mode railscasts-reloaded-theme projectile-rails powerline multiple-cursors markdown-mode magit js2-mode helm-projectile helm-ag haskell-mode haml-mode guru-mode flycheck fill-column-indicator exec-path-from-shell enh-ruby-mode emmet-mode dash-at-point crux coffee-mode beacon auto-complete anzu ack ace-window)))
-
 
 (provide 'custom)
 
 ;;; custom.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zenburn-theme yasnippet yaml-mode websocket web-mode use-package smartparens rspec-mode request rainbow-mode railscasts-reloaded-theme projectile-rails powerline org-bullets oauth2 multiple-cursors markdown-mode magit js2-mode helm-projectile helm-ag haskell-mode haml-mode guru-mode flycheck fill-column-indicator exec-path-from-shell enh-ruby-mode emojify emmet-mode dash-at-point crux coffee-mode circe beacon auto-complete anzu alert ack ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
